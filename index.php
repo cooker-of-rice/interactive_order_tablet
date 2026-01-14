c8974c7a9dfa9f3daab1412122a93f6131ba6590
Rice guy <89035491+cooker-of-rice@users.noreply.github.com>
2026-01-14 16:04:35 +0100

    preview

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo isset($pageTitle) ? $pageTitle : 'Konfigurátor'; ?></title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</head>
+<body>
